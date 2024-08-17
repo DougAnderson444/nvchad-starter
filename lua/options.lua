0df2dde4d1a4e051d8d644a079eb91d46e61b380
Doug Anderson444 <douganderson444@gmail.com>
2024-08-17 11:04:53 -0300

    insert onEnter `term://`

--- lua/options.lua
+++ lua/options.lua
@@ -6,3 +6,10 @@ require "nvchad.options"
 -- o.cursorlineopt ='both' -- to enable cursorline!
 
 vim.diagnostic.config { float = { border = "rounded", source = "if_many" } }
+
+vim.api.nvim_create_autocmd("WinEnter", {
+  pattern = "term://*",
+  callback = function()
+    vim.cmd "startinsert"
+  end,
+})

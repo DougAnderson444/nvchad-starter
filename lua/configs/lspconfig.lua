80dad5a767c326cd9c120d4782050e4962d29756
Doug Anderson444 <douganderson444@gmail.com>
2024-11-02 12:50:10 -0300

    use pest_ls (pest-language-server)

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -5,7 +5,7 @@ local lspconfig = require "lspconfig"
 
 -- EXAMPLE
 -- taplo was installed via `cargo install`, so mason is not needed for it
-local servers = { "html", "cssls", "taplo", "svelte", "pest" }
+local servers = { "html", "cssls", "taplo", "svelte", "pest_ls" }
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config

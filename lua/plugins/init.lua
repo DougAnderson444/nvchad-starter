169808d931ef5099d9dbf2f7186b09d631cc0b0c
Doug Anderson444 <douganderson444@gmail.com>
2025-09-25 17:21:13 -0300

    base46 v3.0, plenary

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,4 +1,22 @@
 return {
+
+  "nvim-lua/plenary.nvim",
+
+  {
+    "nvchad/ui",
+    config = function()
+      require "nvchad"
+    end,
+  },
+
+  {
+    "nvchad/base46",
+    lazy = true,
+    branch = "v3.0",
+    build = function()
+      require("base46").load_all_highlights()
+    end,
+  },
   -- env loading
   {
     "tpope/vim-dotenv",
